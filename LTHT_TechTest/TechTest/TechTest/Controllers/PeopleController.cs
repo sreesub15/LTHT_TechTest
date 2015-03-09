483ad2d932fb2029f24fb61e138b39292c31c7d3
sreesub15 <sreedevi.sub@gmail.com>
2015-03-09 04:43:52 +0000

    LTHT Technical Test

---
+++ LTHT_TechTest/TechTest/TechTest/Controllers/PeopleController.cs
@@ -0,0 +1,126 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace TechTest.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        private readonly TechTestDB _db = new TechTestDB();
+
+        // GET: api/People
+        public IQueryable<Person> GetPeople()
+        {
+            return _db.People.Include(p => p.Colours).OrderBy(p => p.FirstName);
+        }
+
+        // GET: api/People/5
+        [ResponseType(typeof (Person))]
+        public IHttpActionResult GetPerson(int id)
+        {
+            Person person = _db.People.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // PUT: api/People/5
+        [ResponseType(typeof (void))]
+        public IHttpActionResult PutPerson(int id, Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != person.PersonId)
+            {
+                return BadRequest();
+            }
+
+            var dbPerson = _db.People.Include(c => c.Colours).Single(p => p.PersonId == person.PersonId);
+
+            // remove colours that are no longer liked
+            foreach (var colour in dbPerson.Colours.ToList().Where(colour => !person.ColourIDs.Contains(colour.ColourId)))
+            {
+                dbPerson.Colours.Remove(colour);
+            }
+
+            // add favourite colours
+            foreach (var colour in person.ColourIDs.Select(colourId => _db.Colours.Find(colourId)).Where(colour => colour != null))
+            {
+                dbPerson.Colours.Add(colour);
+            }
+
+            _db.Entry(dbPerson).CurrentValues.SetValues(person);
+
+            _db.Entry(dbPerson).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/People
+        [ResponseType(typeof (Person))]
+        public IHttpActionResult PostPerson(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.People.Add(person);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new {id = person.PersonId}, person);
+        }
+
+        // DELETE: api/People/5
+        [ResponseType(typeof (Person))]
+        public IHttpActionResult DeletePerson(int id)
+        {
+            Person person = _db.People.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _db.People.Remove(person);
+            _db.SaveChanges();
+
+            return Ok(person);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _db.People.Count(e => e.PersonId == id) > 0;
+        }
+    }
+}
---
+++ LTHT_TechTest/TechTest/TechTest/KnockoutModels/PersonList.js
@@ -0,0 +1,81 @@
+﻿function PersonList() {
+    var self = this;
+
+    self.people = ko.observableArray([]);
+
+    self.getPeople = function () {
+        self.people.removeAll();
+
+        $.getJSON('/api/people', function (data) {
+            $.each(data, function (key, value) {
+                self.people.push(new Person(value));
+            });
+        });
+    };
+}
+
+function yesNo(val) {
+    return val ? "yes" : "no";
+}
+
+function Person(value) {
+    var self = this;
+
+    self.PersonId = value.PersonId;
+    self.FirstName = value.FirstName;
+    self.LastName = value.LastName;
+    self.IsAuthorised = value.IsAuthorised;
+    self.IsEnabled = value.IsEnabled;
+
+    var colours = [];
+    self.ColourIDs = ko.observableArray();
+    $.each(value.Colours, function (key, v) {
+        colours.push(v.Name);
+        self.ColourIDs.push(v.ColourId + '');
+    });
+    self.ColourString = colours.join(", ");
+
+    self.AuthorisedClass = ko.pureComputed(function() {
+        return yesNo(self.IsAuthorised);
+    }, self);
+
+    self.EnabledClass = ko.pureComputed(function () {
+        return yesNo(self.IsEnabled);
+    }, self);
+
+    self.PalindromeClass = ko.pureComputed(function () {
+        return yesNo(isPalindrome(value.FirstName + value.LastName));
+    }, self);
+
+    function isPalindrome(word) {
+        word = word.replace(" ", "").toLowerCase();
+        return word == word.split('').reverse().join('');
+    }
+
+    self.FullName = ko.pureComputed(function () {
+        return self.FirstName + " " + self.LastName;
+    }, self);
+
+    self.cancelChanges = function () {
+        viewModel.showPeopleList();
+    };
+
+    self.saveChanges = function () {
+        var json = ko.toJSON(this);
+
+        delete json.AuthorisedClass;
+        delete json.EnabledClass;
+        delete json.FullName;
+
+        $.ajax({
+            url: '/api/people/' + this.PersonId,
+            type: 'PUT',
+            contentType: "application/json; charset=utf-8",
+            data: json,
+            success: function () {
+                viewModel.showPeopleList();
+            }
+        });
+    };
+
+}
